d5d4a592f03a6b164204e411d6b603efa17780cd
zanadoman <domanseven@gmail.com>
2024-09-12 14:51:15 +0200

    port argument added

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,12 @@
 use axum::{routing, Router};
+use std::env::args;
 use tokio::net::TcpListener;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::fmt;
 
 use vicc_explorer::routes::health_check;
+
+const DEFAULT_PORT: u16 = 8080;
 
 #[tokio::main]
 async fn main() {
@@ -13,9 +16,16 @@ async fn main() {
         .route("/health_check", routing::get(health_check::get))
         .layer(TraceLayer::new_for_http());
 
-    let listener = TcpListener::bind("0.0.0.0:3000")
-        .await
-        .expect("Failed to bind to address.");
+    let listener = TcpListener::bind(format!(
+        "0.0.0.0:{}",
+        args().nth(1).unwrap_or(DEFAULT_PORT.to_string())
+    ))
+    .await
+    .expect("Failed to bind to address.");
+    println!(
+        "Listening on {:?}",
+        listener.local_addr().expect("Address resolution failed.")
+    );
 
     axum::serve(listener, app)
         .await
