f756cd23e05edf5281cc17d4b08332ada32ec56b
zanadoman <domanseven@gmail.com>
2024-09-17 21:36:08 +0200

    backend database

---
+++ backend/jokedb.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS saved;
+DROP TABLE IF EXISTS votes;
+DROP TABLE IF EXISTS jokes;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    name VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(50) NOT NULL,
+    admin BOOL NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE jokes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(50) NOT NULL,
+    content VARCHAR(500) NOT NULL UNIQUE,
+    FOREIGN KEY (user_name) REFERENCES users(name)
+);
+
+CREATE TABLE votes (
+    user_name VARCHAR(50) NOT NULL,
+    joke_id INT NOT NULL,
+    vote INT NOT NULL DEFAULT 0,
+    FOREIGN KEY (user_name) REFERENCES users(name),
+    FOREIGN KEY (joke_id) REFERENCES jokes(id),
+    PRIMARY KEY (user_name, joke_id)
+);
+
+CREATE TABLE saved (
+    user_name VARCHAR(50) NOT NULL,
+    joke_id INT NOT NULL,
+    FOREIGN KEY (user_name) REFERENCES users(name),
+    FOREIGN KEY (joke_id) REFERENCES jokes(id),
+    PRIMARY KEY (user_name, joke_id)
+);
+
+SHOW TABLES;
