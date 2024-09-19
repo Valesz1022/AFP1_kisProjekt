eea0afc15beeca4fda627c1aba336b00f03976c6
zanadoman <domanseven@gmail.com>
2024-09-19 09:38:32 +0200

    users route completed

--- backend/src/routes.rs
+++ backend/src/routes.rs
@@ -4,10 +4,17 @@ use std::sync::Arc;
 
 pub mod health_check;
 pub mod saved;
+pub mod users;
 
 pub fn router() -> Router<Arc<AppState>> {
     Router::new()
         .route("/health_check", routing::get(health_check::get))
+        .route(
+            "/users",
+            routing::get(users::get)
+                .put(users::put)
+                .delete(users::delete),
+        )
         .route(
             "/saved",
             routing::get(saved::get)
--- backend/src/routes/users.rs
+++ backend/src/routes/users.rs
@@ -1,8 +1,8 @@
-use crate::{models::Joke, AppState};
+use crate::{models::User, AppState};
 use axum::{
     extract::{Query, State},
     http::StatusCode,
-    response::{IntoResponse, Json},
+    response::IntoResponse,
 };
 use sqlx::{query, query_as, MySql};
 use std::{collections::HashMap, sync::Arc};
@@ -11,24 +11,21 @@ pub async fn get(
     State(appstate): State<Arc<AppState>>,
     Query(params): Query<HashMap<String, String>>,
 ) -> impl IntoResponse {
-    match query_as::<MySql, Joke>(
-        "SELECT 
-            jokes.id, 
-            jokes.user_name, 
-            jokes.content,
-            COALESCE(CAST(SUM(votes.vote) AS SIGNED), 0) AS votes
-        FROM saved
-        INNER JOIN users ON saved.user_name = users.name
-        INNER JOIN jokes ON saved.joke_id = jokes.id
-        LEFT JOIN votes ON votes.joke_id = jokes.id
-        WHERE users.name = ?
-        GROUP BY jokes.id, jokes.user_name, jokes.content;",
+    match query_as::<MySql, User>(
+        "SELECT * FROM users
+        WHERE name = ? AND password = ?;",
     )
-    .bind(&Some(params.get("user_name")))
+    .bind(&Some(params.get("name")))
+    .bind(&Some(params.get("password")))
     .fetch_all(&appstate.connection_pool)
     .await
     {
-        Ok(jokes) => (StatusCode::OK, Json(jokes)).into_response(),
+        Ok(users) => if users.len() == 0 {
+            StatusCode::UNAUTHORIZED
+        } else {
+            StatusCode::OK
+        }
+        .into_response(),
         Err(error) => (StatusCode::INTERNAL_SERVER_ERROR, error.to_string())
             .into_response(),
     }
@@ -38,9 +35,9 @@ pub async fn put(
     State(appstate): State<Arc<AppState>>,
     Query(params): Query<HashMap<String, String>>,
 ) -> impl IntoResponse {
-    match query("INSERT INTO saved (user_name, joke_id) VALUES (?, ?);")
-        .bind(&Some(params.get("user_name")))
-        .bind(&Some(params.get("joke_id")))
+    match query("INSERT INTO users (name, password) VALUES (?, ?);")
+        .bind(&Some(params.get("name")))
+        .bind(&Some(params.get("password")))
         .execute(&appstate.connection_pool)
         .await
     {
@@ -53,9 +50,8 @@ pub async fn delete(
     State(appstate): State<Arc<AppState>>,
     Query(params): Query<HashMap<String, String>>,
 ) -> impl IntoResponse {
-    match query("DELETE FROM saved WHERE user_name = ? AND joke_id = ?;")
-        .bind(&Some(params.get("user_name")))
-        .bind(&Some(params.get("joke_id")))
+    match query("DELETE FROM users WHERE name = ?;")
+        .bind(&Some(params.get("name")))
         .execute(&appstate.connection_pool)
         .await
     {
