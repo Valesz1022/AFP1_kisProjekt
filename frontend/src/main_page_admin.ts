ae9c04fafa178291e1c9b25121ba0d781ae5f0ac
Gunics Roland <grolcsi09@gmail.com>
2024-10-02 23:09:36 +0200

    Content security policy added and delete joke implementation

--- frontend/src/main_page_admin.ts
+++ frontend/src/main_page_admin.ts
@@ -1,20 +1,20 @@
 let main_page_admin_elements: {
     posts_container: HTMLDivElement
-    delete_button: HTMLButtonElement
-    up_vote_button: HTMLButtonElement
-    down_vote_button: HTMLButtonElement
-    new_post_button: HTMLButtonElement
+    delete_buttons: HTMLCollection
+    up_vote_buttons: HTMLCollection
+    down_vote_buttons: HTMLCollection
+    new_post_buttons: HTMLButtonElement
     logout_button: HTMLButtonElement
 }
 
 window.addEventListener('load', () => {
     main_page_admin_elements =  {
         posts_container: document.getElementById('posts_container') as HTMLDivElement,
-        delete_button: document.getElementById('delete_buuton') as HTMLButtonElement,
-        up_vote_button: document.getElementById('up_vote_button') as HTMLButtonElement,
-        down_vote_button: document.getElementById('down_vote_button') as HTMLButtonElement,
-        new_post_button: document.getElementById('new_post_button') as HTMLButtonElement,
-        logout_button: document.getElementById('logout_button') as HTMLButtonElement
+        delete_buttons: document.getElementsByClassName('fa-trash') as HTMLCollection,
+        up_vote_buttons: document.getElementsByClassName('fa-arrow-up') as HTMLCollection,
+        down_vote_buttons: document.getElementsByClassName('fa-arrow-down') as HTMLCollection,
+        new_post_buttons: document.getElementById('new_post_button') as HTMLButtonElement,
+        logout_button:document.getElementById('logout_button') as HTMLButtonElement
     }
 
     get_jokes();
@@ -23,20 +23,29 @@ window.addEventListener('load', () => {
         logout();
     })
 
-    main_page_admin_elements.delete_button.addEventListener('click', () => {
-        //delete_joke();
+    Array.from(main_page_admin_elements.delete_buttons).forEach((delete_button) => {
+        let joke_id = delete_button.getAttribute('name');
+        delete_button.addEventListener('click', () => {
+            if(joke_id){
+                delete_joke(joke_id);
+            }
+        })
     })
+    
 })
 
 async function get_jokes() {
+    console.log("viccek lekérése");
     let response = await fetch(`${SERVER}`, {
         method: "GET"
     });
 
     let jokes = await response.json();
+    console.log("megjöttek a viccek" + response.status);
 
     switch(response.status){
         case 200:
+            console.log("sikeres lekérés");
             jokes.json.forEach((joke: Types.Joke) => {
                 //a divek létrehozása és appendelése a main_page_user.html alapján
                 let post = document.createElement('div');
@@ -69,6 +78,7 @@ async function get_jokes() {
         
                 let delete_img = document.createElement('i');
                 delete_img.classList.add('fa-solid fa-trash', 'fa-2x');
+                delete_img.setAttribute('name', `${joke.id}`);
         
                 post_left_top.appendChild(post_left_top_left);
                 post_left_top.appendChild(post_left_top_right);
